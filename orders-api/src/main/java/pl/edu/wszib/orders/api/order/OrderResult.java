6ad4bfdf2694d1d41d7ac5b1fedf6f8ebc1185e3
karol82 <karol82@int.pl>
2023-01-14 15:01:45 +0100

    2023.01.14

--- orders-api/src/main/java/pl/edu/wszib/orders/api/order/OrderResult.java
+++ orders-api/src/main/java/pl/edu/wszib/orders/api/order/OrderResult.java
@@ -3,7 +3,7 @@ package pl.edu.wszib.orders.api.order;
 public interface OrderResult {
     OrderApi success();
     OrderError error();
-
+    //
     default boolean isSuccess() {
         return success() != null;
     }
